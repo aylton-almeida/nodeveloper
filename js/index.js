19d203e8a43e66beedf5e8439554a4797a63ed1c
stevreeper <ayltonalmeida2@gmail.com>
2019-03-11 10:39:48 -0300

    add animação e texto principal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $('#block1').on("mouseenter mouseleave", (e)=>{
-	let size = e.type === "mouseenter"? "103% 103%" : "100% 100%";
-	$('#block1').css("background-size", size);
+	let size = e.type === "mouseenter"? 3 : 2.5;
+	$('.main-title').animate({fontSize: size + "em"});
+	$('.main-subtitle').animate({	fontSize: (size == 3? 1.8 : 1.5) + "em"});
 })

02c51f4a01d372a195f1618731c04f67d98b6e79
stevreeper <ayltonalmeida2@gmail.com>
2019-03-16 12:15:09 -0300

    retirado section 2

--- js/index.js
+++ js/index.js
@@ -4,26 +4,3 @@ $('#section1').on("mouseenter mouseleave", (e)=>{
 	$('.main-title').animate({fontSize: size + "em"});
 	$('.main-subtitle').animate({	fontSize: (size == 3? 1.8 : 1.5) + "em"});
 })
-
-//Scroll animation
-$('.scrollTo').click(function() {
-	let section = $(this).attr("href");
-	if ($(section).length) {
-		let offSet = $(section).offset().top;
-		$('html,body').animate({
-			scrollTop: offSet
-		}, 500)
-	}
-	return false;
-})
-
-//Change active link when scrolling
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-		let x = $("[id^='section']")
-		console.log(typeof x);
-		console.log($(x[1]).offset().top);
-    if ($(x[1]).offset().top == scroll) {
-			console.log("ok");
-    }
-});

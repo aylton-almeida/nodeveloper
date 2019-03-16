01fb5fd964f730f3f68322ac34704d78e97c2ad3
Aylton Almeida <ayltonalmeida2@gmail.com>
2019-03-16 00:43:56 -0300

    problema no viewPort

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$('#block1').on("mouseenter mouseleave", (e)=>{
+$('#section1').on("mouseenter mouseleave", (e)=>{
 	let size = e.type === "mouseenter"? 3 : 2.5;
 	$('.main-title').animate({fontSize: size + "em"});
 	$('.main-subtitle').animate({	fontSize: (size == 3? 1.8 : 1.5) + "em"});
